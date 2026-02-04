dbd50628412f06bd47147e62c4e4b753fb2466ac
Priyansh Kayastha <priyansh.kayastha2004@gmail.com>
2026-02-04 10:15:35 +0530

    Notification issue

--- services/notification-service/src/main.ts
+++ services/notification-service/src/main.ts
@@ -1,15 +1,43 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { Transport } from '@nestjs/microservices';
+// import { ConfigService } from '@nestjs/config';
+
+// async function bootstrap() {
+//   const appContext = await NestFactory.createApplicationContext(AppModule);
+//     const config = appContext.get(ConfigService);
+
+//   const app = await NestFactory.createMicroservice(AppModule, {
+//     transport: Transport.RMQ,
+//     options: {      
+//       urls: [config.getOrThrow<string>('RABBITMQ_URL')],
+//       queue: config.getOrThrow<string>('RABBITMQ_QUEUE'),
+//       queueOptions: {
+//         durable: true,
+//       },
+//     },
+//   });
+
+  
+//   await app.listen(); 
+//   console.log('Notification microservice is listening to RabbitMQ');
+// }
+// bootstrap();
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Transport } from '@nestjs/microservices';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const appContext = await NestFactory.createApplicationContext(AppModule);
-    const config = appContext.get(ConfigService);
+  const app = await NestFactory.create(AppModule);
 
-  const app = await NestFactory.createMicroservice(AppModule, {
+  const config = app.get(ConfigService);
+
+  app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
-    options: {      
+    options: {
       urls: [config.getOrThrow<string>('RABBITMQ_URL')],
       queue: config.getOrThrow<string>('RABBITMQ_QUEUE'),
       queueOptions: {
@@ -18,7 +46,14 @@ async function bootstrap() {
     },
   });
 
-  await app.listen(); 
+  await app.startAllMicroservices();
+
+  // REQUIRED for Render free Web Service
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`HTTP server listening on port ${port}`);
   console.log('Notification microservice is listening to RabbitMQ');
 }
+
 bootstrap();
