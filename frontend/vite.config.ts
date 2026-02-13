8be4e97231a2a125de415335c2f6550c4473df18
Priyansh Kayastha <priyansh.kayastha2004@gmail.com>
2026-02-13 16:15:44 +0530

    fix test issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@
 
 
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
