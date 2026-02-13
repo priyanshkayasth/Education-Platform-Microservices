b673dc23adfa3790b840a948ce99e88819ce3d39
Priyansh Kayastha <priyansh.kayastha2004@gmail.com>
2026-02-13 17:02:25 +0530

    login issue3

--- services/api-gateway/src/routes/auth.routes.ts
+++ services/api-gateway/src/routes/auth.routes.ts
@@ -46,22 +46,22 @@ export class AuthRoutes {
     );
   }
 
-//  @Get('google')
-// google(@Res() res: any) {
-//   return res.redirect(
-//     `${this.servicesConfig.authService}/auth/google`
-//   );
-// }
+ @Get('google')
+google(@Res() res: any) {
+  return res.redirect(
+    `${this.servicesConfig.authService}/auth/google`
+  );
+}
 
 
-// @Get('google/callback')
-// googleCallback(@Res() res: any, @Req() req) {
-//   const query = new URLSearchParams(req.query as any).toString();
+@Get('google/callback')
+googleCallback(@Res() res: any, @Req() req) {
+  const query = new URLSearchParams(req.query as any).toString();
 
-//   return res.redirect(
-//     `${this.servicesConfig.authService}/auth/google/callback?${query}`
-//   );
-// }
+  return res.redirect(
+    `${this.servicesConfig.authService}/auth/google/callback?${query}`
+  );
+}
 
 
   // PROTECTED ROUTES
