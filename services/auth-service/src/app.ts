83369f8b797c103a9db3b1c0a3e80b7cbb49230b
Priyansh Kayastha <priyansh.kayastha2004@gmail.com>
2026-02-13 16:53:55 +0530

    Login issue2

--- services/auth-service/src/app.ts
+++ services/auth-service/src/app.ts
@@ -10,7 +10,13 @@ import passport from './config/passport.js'
 
 const app=express()
 
-app.use(cors())
+const allowedOrigins = process.env.CORS_ORIGIN?.split(",") || [];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
+
 app.use(cookieParser()); 
 
 app.use(express.json())
