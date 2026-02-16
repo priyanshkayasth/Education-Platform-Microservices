b763e304782ad74e37e072b3ee8c4d05e7abe814
Priyansh Kayastha <priyansh.kayastha2004@gmail.com>
2026-02-16 23:23:42 +0530

    OAuth

--- frontend/src/pages/OAuthSuccess.tsx
+++ frontend/src/pages/OAuthSuccess.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api/axios";
+import { useAuth } from "../context/AuthContext";
 
 export default function OAuthSuccess() {
   const navigate = useNavigate();
+  const {refetchUser}=useAuth()
 
   useEffect(() => {
     const token = new URLSearchParams(window.location.search).get("token");
@@ -16,6 +18,8 @@ export default function OAuthSuccess() {
     const completeLogin = async () => {
       try {
         await api.post("/auth/oauth-login", { token });
+        await new Promise(resolve => setTimeout(resolve,100))
+        await refetchUser()
         navigate("/", { replace: true });
       } catch (err) {
         console.error("OAuth login failed", err);
