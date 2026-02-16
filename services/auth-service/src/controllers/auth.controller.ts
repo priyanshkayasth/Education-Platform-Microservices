6a139ce13a2463a8ba8b5add823c3bd449df1f43
Priyansh Kayastha <priyansh.kayastha2004@gmail.com>
2026-02-16 13:15:46 +0530

    Cookie issue

--- services/auth-service/src/controllers/auth.controller.ts
+++ services/auth-service/src/controllers/auth.controller.ts
@@ -100,6 +100,8 @@ export const googleCallback = async (req: Request, res: Response) => {
       secure:true,
       // secure:false,
       sameSite: "none", 
+      domain: ".onrender.com",   //  REQUIRED for Google OAuth
+
       maxAge: 15 * 60 * 1000, // 15 minutes,
      path:'/'
 
