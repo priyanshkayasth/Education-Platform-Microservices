48c4e4b01ff8633d4c03c4b75e83a9613d559739
Priyansh Kayastha <priyansh.kayastha2004@gmail.com>
2026-02-17 11:35:38 +0530

    auth

--- frontend/src/routes/HomeRedirect.tsx
+++ frontend/src/routes/HomeRedirect.tsx
@@ -2,7 +2,10 @@ import { useAuth } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
 
 export default function HomeRedirect() {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
+
+    if (loading) return <div>Loading...</div>;
+
 
   if (!user) return <Navigate to="/login" replace />;
 
